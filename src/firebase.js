2f60464a37208f11d4a080e5ddf26be929afed42
Kuldeep Khare <Kuldeep.5.Khare@NIIT-Tech.com>
2020-10-07 21:53:57 +0530

    KK: hide firebase details

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCw8Ht9pD6mRERLVVlmWOAZt5FxIgeiJ5E",
-    authDomain: "slack-378f0.firebaseapp.com",
-    databaseURL: "https://slack-378f0.firebaseio.com",
-    projectId: "slack-378f0",
-    storageBucket: "slack-378f0.appspot.com",
-    messagingSenderId: "177662771915",
-    appId: "1:177662771915:web:8c265fdc2079862660514e",
-    measurementId: "G-82LX8BLQSJ"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp((firebaseConfig));
